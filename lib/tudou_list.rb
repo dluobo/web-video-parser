beabe1bf0a35033c6e3495cb4678bc131f593f15
Kaid <kaid@kaid.me>
2013-10-10 11:27:14 +0800

    refactored tudou_list

--- lib/tudou_list.rb
+++ lib/tudou_list.rb
@@ -1,104 +1,43 @@
 # -*- coding: utf-8 -*-
 module VideoParser
   class TudouList
-    class Item
-      attr_reader :meta
-
-      def initialize(meta)
-        @meta = meta
-        define_methods
-      end
-
-      def desc
-        # @desc ||= desc_json["message"]["description"]
-        # 0718 性能过低。暂时先不使用
-        ""
-      end
-      
-      def url
-        "http://www.tudou.com/programs/view/#{code}/"
-      end
-
-      def cover_url
-        self.picUrl
-      end
-
-      private
-
-      def desc_json
-        url = "http://www.tudou.com/playlist/service/getItemDetail.html?code=#{code}"
-        @desc_json ||= Parser.new(url, :json).data
-      end
-
-      def define_methods
-        meta.each do |key, value|
-          define_singleton_method key.underscore do
-            meta[key]
-          end
-        end
-      end
-    end
-
-    class List
-      attr_reader :url
-
-      def initialize(url)
-        @url = url
-        @meta_parser = Parser.new(@url, :xml)
-      end
-
-      def meta
-        @response ||= @meta_parser.data
-      end
-
-      def title
-        @title ||= meta.at_css('.sec_2 .caption').content
-      end
-
-      def lid
-        @lid ||= @meta_parser.raw.match(/var\slid\s=\s'(\d*)';/)[1].to_i
-      end
-
-      def count
-        @count ||= meta.css('.mod_box_bd .summary.fix')[1].css('i')[0].content.to_i
-      end
-
-      def videos
-        @videos ||= list_json['message']['items'].map do |data|
-          Item.new(data)
-        end
-      end
-
-      private
-
-      def list_json
-        @list_json ||= Parser.new(list_json_url, :json).data
-      end
-
-      def list_json_url
-        "http://www.tudou.com/plcover/coverPage/getIndexItems.html?lid=#{lid}&pageSize=#{count}"
-      end
-    end
+    attr_reader :url
 
     def initialize(url)
       @url = url
-      # url like
-      # http://www.tudou.com/plcover/M9ovmjs6fkw/
+      @meta_parser = Parser.new(@url, :xml)
     end
 
-    def parse
-      list = List.new(@url)
+    def meta
+      @response ||= @meta_parser.data
+    end
 
-      title = list.title
+    def title
+      @title ||= meta.at_css('.sec_2 .caption').content
+    end
 
-      videos = list.items.map {|item|
-        { :title => item.title, :url => item.url }
-      }
+    def lid
+      @lid ||= @meta_parser.raw.match(/var\slid\s=\s'(\d*)';/)[1].to_i
+    end
 
-      return {
-        :name => title,
-        :videos => videos
-      }
+    def count
+      @count ||= meta.css('.mod_box_bd .summary.fix')[1].css('i')[0].content.to_i
+    end
+
+    def videos
+      @videos ||= list_json['message']['items'].map do |data|
+        {title: data["title"], cover_url: data["picUrl"], url: "http://www.tudou.com/programs/view/#{data["code"]}/"}
+      end
+    end
+
+    private
+
+    def list_json
+      @list_json ||= Parser.new(list_json_url, :json).data
+    end
+
+    def list_json_url
+      "http://www.tudou.com/plcover/coverPage/getIndexItems.html?lid=#{lid}&pageSize=#{count}"
     end
   end
 end
